716f14ef571d1654b91767624c510450b2f8bb63
Your Name <you@example.com>
2020-07-11 09:22:24 +0000

    fixed tests for jenkins use

--- app/tests/test_mock.py
+++ app/tests/test_mock.py
@@ -1,12 +1,23 @@
 from unittest.mock import patch
 from flask import url_for
 from flask_testing import TestCase
-
-from app1 import app
+from os import getenv
+from app1 import app, db
 
 class TestBase(TestCase):
     def create_app(self):
+        config_name = 'testing'
+        app.config.update(SQLALCHEMY_DATABASE_URI=getenv('TESTING_URI'),
+        WTF_CSRF_ENABLED=False,
+        DEBUG=True)
         return app
+    def setUp(self):
+        db.session.commit()
+        db.drop_all()
+        db.create_all()
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
 class TestResponse(TestBase):
 
